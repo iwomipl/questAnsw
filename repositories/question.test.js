ca23addd8b4d910112a65d55684dfe1dc4709e4b
Iwo Matecki <iwo@iwomi.pl>
2022-10-21 22:53:20 +0200

    Finish validators.test.js and some fixes to question.test.js

--- repositories/question.test.js
+++ repositories/question.test.js
@@ -3,19 +3,50 @@ const { faker } = require('@faker-js/faker')
 const { makeQuestionRepository } = require('./question')
 const { ValidationError } = require('../utils/errors')
 
-describe('question repository', () => {
-  const TEST_QUESTIONS_FILE_PATH = 'test-questions.json'
-  const uuid = faker.datatype.uuid()
-  let questionRepo
+const TEST_QUESTIONS_FILE_PATH = 'test-questions.json'
+const uuid = faker.datatype.uuid()
+let questionRepo
+let testQuestions
+let testQuestion
 
-  beforeAll(async () => {
-    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify([]))
-    questionRepo = makeQuestionRepository(TEST_QUESTIONS_FILE_PATH)
-  })
+beforeAll(async () => {
+  await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify([]))
+  questionRepo = makeQuestionRepository(TEST_QUESTIONS_FILE_PATH)
+  testQuestion = [
+    {
+      id: uuid,
+      summary: 'What is my name?',
+      author: 'Jack London',
+      answers: [{
+        'id': 'd498b0a4-6be3-4354-a3bc-87673aca0f31',
+        'author': 'Mr Fantastic',
+        'summary': 'Lack Jondon.'
+      }]
+    }
+  ]
 
-  afterAll(async () => {
-    await rm(TEST_QUESTIONS_FILE_PATH)
-  })
+  testQuestions = [
+    {
+      id: uuid,
+      summary: 'What is my name?',
+      author: 'Jack London',
+      answers: []
+    },
+    {
+      id: faker.datatype.uuid(),
+      summary: 'Who are you?',
+      author: 'Tim Doods',
+      answers: []
+    }
+  ]
+})
+
+afterAll(async () => {
+  await rm(TEST_QUESTIONS_FILE_PATH)
+})
+
+describe('questions in question repository', () => {
+
   /**-----------------------  getQuestions  -----------------------------*/
   test('getQuestions() should return a list of 0 questions', async () => {
     expect(await questionRepo.getQuestions()).toHaveLength(0)
@@ -27,58 +58,42 @@ describe('question repository', () => {
   })
 
   test('getQuestions() should return a list of 2 questions', async () => {
-    const testQuestions = [
-      {
-        id: uuid,
-        summary: 'What is my name?',
-        author: 'Jack London',
-        answers: []
-      },
-      {
-        id: faker.datatype.uuid(),
-        summary: 'Who are you?',
-        author: 'Tim Doods',
-        answers: []
-      }
-    ]
 
     await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+    const questions = await questionRepo.getQuestions()
 
-    expect(await questionRepo.getQuestions()).toHaveLength(2)
+    expect(questions).toHaveLength(2)
+    expect(questions).toEqual(expect.objectContaining(questions))
   })
 
 
-  test('getQuestionById() should return 1 question', async () => {
+  test('getQuestionById() should return 1 question that is the same question', async () => {
     const testedQuestion = await questionRepo.getQuestionById(uuid)
 
     expect(testedQuestion).toHaveLength(1)
-    expect(testedQuestion).toEqual(expect.objectContaining([{
-      id: uuid,
-      summary: 'What is my name?',
-      author: 'Jack London',
-      answers: []
-    }]))
+    expect(testedQuestion).toEqual(expect.objectContaining(testedQuestion))
   })
 
   test('getQuestionById() should throw an Error if have no arguments', async () => {
-    await expect(async () => await questionRepo.getQuestionById()).rejects.toThrow(ValidationError)
+    await expect(async () => await questionRepo.getQuestionById()).rejects.toThrow(new ValidationError('Sorry, this is not UUID.'))
   })
 
   test('getQuestionById() should throw an Error if id is not uuid', async () => {
     const questionID = 'random-gibberish'
-    await expect(async () => await questionRepo.getQuestionById(questionID)).rejects.toThrow(ValidationError)
+    await expect(async () => await questionRepo.getQuestionById(questionID)).rejects.toThrow(new ValidationError('Sorry, this is not UUID.'))
   })
 
   /**-----------------------  addQuestions  -----------------------------*/
 
-  //Author argument validation--------------------------------------------
+  /*Author argument validation*/
+
   test('addQuestion() should throw an Error if "author" argument is to short', async () => {
-    await expect(async () => await questionRepo.addQuestion('a', 'Is It?')).rejects.toThrow('Auhtor\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.')
+    await expect(async () => await questionRepo.addQuestion('a', 'Is It?')).rejects.toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
   })
 
   test('addQuestion() should throw an Error if "author" argument is to long', async () => {
     await expect(async () => await questionRepo.addQuestion(Array(51).fill('a', 0, 51).join('').length, 'Is It?'))
-      .rejects.toThrow('Auhtor\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.')
+      .rejects.toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
   })
 
   test('addQuestion() should throw an Error if "author" argument is undefined', async () => {
@@ -97,8 +112,8 @@ describe('question repository', () => {
     await expect(async () => await questionRepo.addQuestion(Math.round(Math.random() * 105.5), 'Is It?')).rejects.toThrow(ValidationError)
   })
 
+  /* Question argument validation */
 
-  //Question argument validation----------------------------------------------
   test('addQuestion() should throw an Error if "question" argument does Not have question mark at the end', async () => {
     await expect(async () => await questionRepo.addQuestion('author', 'ttt')).rejects.toThrow('All questions should have question mark at the end My Friend. Fix it and send Your question again.')
   })
@@ -128,8 +143,67 @@ describe('question repository', () => {
     await expect(async () => await questionRepo.addQuestion('author', Math.round(Math.random() * 105.5))).rejects.toThrow(ValidationError)
   })
 
+  test('getQuestion() should return testQuestion after adding and reading it back from the file', async () => {
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestion))
+
+    const addedQuestion = await questionRepo.getQuestionById(uuid)
+
+    expect(addedQuestion).toHaveLength(1)
+    expect(addedQuestion).toEqual(expect.objectContaining(testQuestion))
+  })
+
+})
+
+describe('answers in question repository', () => {
 
   /**-----------------------  getAnswers  -----------------------------*/
+  test('getAnswers() should throw an Error if have no arguments', async () => {
+    await expect(async () => await questionRepo.getAnswers()).rejects.toThrow(new ValidationError('Sorry, this is not UUID.'))
+  })
+
+  test('getAnswers() should throw an Error if id is not uuid', async () => {
+    const questionID = 'random-gibberish'
+    await expect(async () => await questionRepo.getAnswers(questionID)).rejects.toThrow(new ValidationError('Sorry, this is not UUID.'))
+  })
+
+  test('getAnswers() should throw an Error if "questionID" argument is undefined', async () => {
+    await expect(async () => await questionRepo.getAnswers(undefined)).rejects.toThrow(new ValidationError('Sorry, this is not UUID.'))
+  })
+
+  test('getAnswers() should throw an Error if "questionId" argument is null', async () => {
+    await expect(async () => await questionRepo.getAnswers(null)).rejects.toThrow(new ValidationError('Sorry, this is not UUID.'))
+  })
+
+  test('getAnswers() should throw an Error if "questionId" argument is Boolean', async () => {
+    await expect(async () => await questionRepo.getAnswers(Boolean(Math.round(Math.random() * 0.5))).rejects.toThrow(new ValidationError('Sorry, this is not UUID.')))
+  })
+
+  test('getAnswers() should throw an Error if "questionId" argument is Number', async () => {
+    await expect(async () => await questionRepo.getAnswers(Math.round(Math.random() * 105.5))).rejects.toThrow(new ValidationError('Sorry, this is not UUID.'))
+  })
+
+  test('getQuestion() should return testQuestion after adding and reading it back from the file', async () => {
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestion))
+
+    const addedQuestion = await questionRepo.getQuestionById(uuid)
+
+    expect(addedQuestion).toHaveLength(1)
+    expect(addedQuestion).toEqual(expect.objectContaining(testQuestion))
+  })
+
+
+  test('getAnswers() should return testQuestion\'s answers', async () => {
+
+    const gotAnswers = await questionRepo.getAnswers(uuid)
+    const answers = [{
+      'id': 'd498b0a4-6be3-4354-a3bc-87673aca0f31',
+      'author': 'Mr Fantastic',
+      'summary': 'Lack Jondon.'
+    }]
+
+    expect(gotAnswers).toHaveLength(1)
+    expect(gotAnswers).toEqual(expect.objectContaining(answers))
+  })
 
 
   /**-----------------------  addAnswers  -----------------------------*/
--- utils/validators.test.js
+++ utils/validators.test.js
@@ -1,5 +1,5 @@
 const { ValidationError } = require('../utils/errors')
-const { uuidValidator, questionValidator } = require('./validators')
+const { uuidValidator, questionValidator, authorValidator, answerValidator } = require('./validators')
 const { faker } = require('@faker-js/faker')
 
 const uuid = faker.datatype.uuid()
@@ -47,7 +47,7 @@ describe('checking validation utils', () => {
     expect(() => questionValidator('ttt')).toThrow('All questions should have question mark at the end My Friend. Fix it and send Your question again.')
   })
 
-  test('questionValidator() should throw an Error if "question" argument does Not have question mark at the end bu in the middle', () => {
+  test('questionValidator() should throw an Error if "question" argument does Not have question mark at the end but has one in the middle', () => {
     expect(() => questionValidator('t?tt')).toThrow('All questions should have question mark at the end My Friend. Fix it and send Your question again.')
   })
 
@@ -89,12 +89,88 @@ describe('checking validation utils', () => {
 
 
   /**-----------------------  answerValidator  -----------------------------*/
+  test('answerValidator() should throw an Error if have no arguments', () => {
+    expect(() => answerValidator()()).toThrow(new ValidationError('Answer should be string between 3 and 150 characters long. Fix it and send Your question again.'))
+  })
 
+  test('answerValidator() should throw an Error if "question" argument is to short', () => {
+    expect(() => answerValidator('t')).toThrow(new ValidationError('Answer should be string between 3 and 150 characters long. Fix it and send Your question again.'))
+  })
+
+  test('answerValidator() should throw an Error if "question" argument is to long', () => {
+    expect(() => answerValidator(`${Array(150).fill('a', 0, 150).join('')}?`))
+      .toThrow(new ValidationError('Answer should be string between 3 and 150 characters long. Fix it and send Your question again.'))
+  })
+
+  test('answerValidator() should throw an Error if uuid is Number', () => {
+    expect(() => answerValidator(Math.round(Math.random() * 105.5))).toThrow(new ValidationError('Answer should be string between 3 and 150 characters long. Fix it and send Your question again.'))
+  })
+
+  test('answerValidator() should throw an Error if uuid is undefined', () => {
+    expect(() => answerValidator(undefined)).toThrow(new ValidationError('Answer should be string between 3 and 150 characters long. Fix it and send Your question again.'))
+  })
+
+  test('answerValidator() should throw an Error if uuid is null', () => {
+    expect(() => answerValidator(null)).toThrow(new ValidationError('Answer should be string between 3 and 150 characters long. Fix it and send Your question again.'))
+  })
+
+  test('answerValidator() should throw an Error if uuid is Boolean', () => {
+    expect(() => answerValidator(Boolean(Math.round(Math.random() * 0.5)))).toThrow(new ValidationError('Answer should be string between 3 and 150 characters long. Fix it and send Your question again.'))
+  })
+
+  test('answerValidator() should throw an Error if uuid is Number', () => {
+    expect(() => answerValidator(Math.round(Math.random() * 105.5))).toThrow(new ValidationError('Answer should be string between 3 and 150 characters long. Fix it and send Your question again.'))
+  })
+
+  test('answerValidator() should return question string if given proper one', () => {
+    const properQuestion = 'Author\'s real name'
+    const functionReturn = answerValidator(properQuestion)
+
+    expect(functionReturn).toEqual(properQuestion)
+  })
 
   /**-----------------------  authorValidator  -----------------------------*/
 
+  test('authorValidator() should throw an Error if have no arguments', () => {
+    expect(() => authorValidator()()).toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
+  })
 
-  /**-----------------------  addAnswers  -----------------------------*/
+  test('authorValidator() should throw an Error if "question" argument is to short', () => {
+    expect(() => authorValidator('t')).toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
+  })
+
+  test('authorValidator() should throw an Error if "question" argument is to long', () => {
+    expect(() => authorValidator(`${Array(51).fill('a', 0, 51).join('')}`))
+      .toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
+  })
+
+  test('authorValidator() should throw an Error if uuid is Number', () => {
+    expect(() => authorValidator(Math.round(Math.random() * 105.5))).toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
+  })
+
+  test('authorValidator() should throw an Error if uuid is undefined', () => {
+    expect(() => authorValidator(undefined)).toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
+  })
+
+  test('authorValidator() should throw an Error if uuid is null', () => {
+    expect(() => authorValidator(null)).toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
+  })
+
+  test('authorValidator() should throw an Error if uuid is Boolean', () => {
+    expect(() => authorValidator(Boolean(Math.round(Math.random() * 0.5)))).toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
+  })
+
+  test('authorValidator() should throw an Error if uuid is Number', () => {
+    expect(() => authorValidator(Math.round(Math.random() * 105.5))).toThrow(new ValidationError('Author\'s credentials should be between 2 and 50 letters long. Fix it and send Your question again.'))
+  })
+
+  test('authorValidator() should return question string if given proper one', () => {
+    const properQuestion = 'Lubricants it\'s always lubricants'
+    const functionReturn = authorValidator(properQuestion)
+
+    expect(functionReturn).toEqual(properQuestion)
+  })
+
 
 
 })
